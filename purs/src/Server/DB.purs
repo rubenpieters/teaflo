7145f0fb133339bcbf5928d673dcc3b0c3896e63
ruben <rubenpietersgithub@gmail.com>
2018-04-07 21:22:25 +0200

    get/put board from DB

--- purs/src/Server/DB.purs
+++ purs/src/Server/DB.purs
@@ -2,20 +2,39 @@ module Server.DB where
 
 import Prelude
 
+import Shared.Board
 import Shared.Solution
 
-import Data.Either (Either(..))
+import Data.Foreign (Foreign)
 
-import Control.Monad.Eff
+import Data.Maybe (Maybe(..))
+import Data.Either (Either(..))
+import Data.Foreign.Class (class Decode, decode)
+import Data.Foreign.Index (readProp)
+import Data.Bifunctor (lmap)
+
+import Control.Monad.Except (runExcept)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Exception (throw)
+import Control.Monad.Aff (Aff)
+
+import Data.Argonaut.Decode.Class (decodeJson)
+import Data.Argonaut.Encode.Class (encodeJson)
+import Data.Argonaut.Core (stringify)
+import Data.Argonaut.Parser (jsonParser)
 
 import Simple.JSON (readJSON)
 
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
-import Database.Postgres (ClientConfig, ConnectionInfo)
+import Database.Postgres (Client, ClientConfig, ConnectionInfo, Query(..))
 import Database.Postgres as PG
+
+import Debug.Trace (traceAny)
+
+import Unsafe.Coerce (unsafeCoerce)
 
 getCurrentTop ::
   Eff _ { top :: Array Int }
@@ -27,6 +46,64 @@ submitSolution ::
   } ->
   Eff _ Unit
 submitSolution { boardId, solution } = pure unit
+
+newtype DBBoard = DBBoard
+  { id :: Int
+  , boardJson :: String
+  }
+
+instance decodeDBBoard :: Decode DBBoard where
+  decode obj = do
+    traceAny obj $ (\_ -> pure unit)
+    id <- decode =<< readProp "id" obj
+    boardJson <- decode =<< readProp "boardJson" obj
+    pure $ DBBoard { id: id, boardJson: boardJson }
+
+-- stored data is exactly the purescript representation
+-- not sure if good idea...
+toBoard :: Foreign -> Board
+toBoard = unsafeCoerce
+
+--toBoard :: Foreign -> Either String Board
+--toBoard obj =
+{-  traceAny obj $ (\_ -> pure unit)
+  boardJson <- lmap show $ runExcept $ decode obj
+  traceAny boardJson $ (\_ -> pure unit)
+  parsedJson <- jsonParser boardJson
+  traceAny parsedJson $ (\_ -> pure unit)
+  decodedJson <- decodeJson parsedJson
+  traceAny decodedJson $ (\_ -> pure unit)
+  pure decodedJson
+-}
+
+putBoard ::
+  { id :: Int, board :: Board } ->
+  Client ->
+  Aff _ Unit
+putBoard { id, board } c = do
+  let query = Query $ "insert into boards values (" <> show id <> ", '" <> stringify (encodeJson board) <> "')"
+  c # PG.execute_ query
+
+{-
+getBoards ::
+  Client ->
+  Aff _ (Array (Either String { id :: Int, board :: Board }))
+getBoards c = do
+  let (query :: Query Foreign) = Query $ "select id, boardJson from boards"
+  dbBoards <- c # PG.query_ query
+  pure (dbBoards <#> toBoard)
+-}
+
+getBoard ::
+  { id :: Int } ->
+  Client ->
+  Aff _ (Either String Board)
+getBoard { id } c = do
+  let (query :: Query Foreign) = Query $ "select boardJson from boards where id = " <> show id
+  dbBoard <- c # PG.queryValue_ query
+  pure $ case dbBoard of
+    Nothing -> Left $ "No board for id " <> show id
+    Just x -> Right $ x # toBoard
 
 --------------------------------------
 -- Connection Info
--- purs/src/Server/DBScripts.purs
+++ purs/src/Server/DBScripts.purs
@@ -2,18 +2,26 @@ module Server.DBScripts where
 
 import Prelude
 
+import Shared.Board
+import Shared.Node
 import Server.DB as DB
 
 import Data.Foreign (Foreign)
 
-import Control.Monad.Aff (launchAff_)
+import Control.Monad.Aff (Aff, launchAff_)
 import Control.Monad.Eff (kind Effect, Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log)
+import Control.Monad.Eff.Exception (EXCEPTION)
+
+import Data.Argonaut.Decode.Class (decodeJson)
+import Data.Argonaut.Encode.Class (encodeJson)
+import Data.Argonaut.Core (stringify)
 
 import Node.ReadLine (prompt, close, setLineHandler, setPrompt,  noCompletion, createConsoleInterface)
 
-import Database.Postgres (DB, Query(..))
+import Node.FS (FS)
+import Database.Postgres (DB, Client, Query(..))
 import Database.Postgres as PG
 
 import Debug.Trace (traceAny)
@@ -30,23 +38,54 @@ main = do
         run s
         prompt interface
 
+testBoard :: Board
+testBoard = Board
+  { nodes:
+    [ Node { id: 0, x: 0.0, y: 0.0, nodeType: Start }
+    , Node { id: 1, x: 4.0, y: 4.0, nodeType: ResourceNode
+        { gain: Resources
+          { growth: 0.0
+          , white: 0
+          , blue: 0
+          , red: 0
+          , green: 1
+          , yellow: 0
+          }
+        , cost: Resources
+          { growth: 0.0
+          , white: 0
+          , blue: 0
+          , red: 0
+          , green: 0
+          , yellow: 0
+          }
+        , name: "green"
+        }}
+    ]
+  }
+
 run :: String -> Eff _ Unit
 run "setup" = setupDB
+run "test1" = log (stringify (encodeJson testBoard))
+run "test2" = onPostgres (\c -> c # DB.putBoard { id: 1, board: testBoard })
+run "test3" = onPostgres (\c -> c # PG.query_ (Query ("select boardJson from boards where id = 1") :: Query Foreign) >>= \x -> traceAny x (\_ -> pure unit))
+run "test4" = onPostgres (\c -> c # DB.getBoard { id: 1 } >>= \x -> traceAny x (\_ -> pure unit) *> liftEff $ log "success")
 run cmd = log ("unknown command " <> cmd)
 
-test :: Eff _ Unit
-test = launchAff_ do
+onPostgres :: forall eff a.
+  (Client -> Aff (db :: DB, exception :: EXCEPTION, fs :: FS | eff) Unit) ->
+  Eff (db :: DB, exception :: EXCEPTION, fs :: FS | eff) Unit
+onPostgres aff = launchAff_ do
   connectionInfo <- liftEff $ DB.connectionInfo
   pool <- liftEff $ PG.mkPool connectionInfo
-  PG.withClient pool $ \c -> do
-    tables <- PG.queryValue_ (Query "select * from pg_catalog.pg_tables" :: Query Foreign) c
-    traceAny tables (\_ -> pure unit)
+  PG.withClient pool $ \c -> aff c
+
+    --tables <- PG.queryValue_ (Query "select * from pg_catalog.pg_tables" :: Query Foreign) c
+    --traceAny tables (\_ -> pure unit)
 
 setupDB :: Eff _ Unit
-setupDB = launchAff_ do
-  connectionInfo <- liftEff $ DB.connectionInfo
-  pool <- liftEff $ PG.mkPool connectionInfo
-  PG.withClient pool $ \c -> do
+setupDB = do
+  onPostgres $ \c -> do
     PG.execute_ (Query setup) c
 
 setup :: String
