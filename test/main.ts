f35c9fb7ab0cac290a856ef6bc1e778bfcac21ae
rubenpieters <rubenpietersgithub@gmail.com>
2018-07-28 11:10:26 +0200

    start goldentest function

--- test/main.ts
+++ test/main.ts
@@ -1,4 +1,40 @@
 import iassign from "immutable-assign";
 import * as fs from "fs";
+import * as path from "path";
 
-//fs.readFile("/")
+type TestResult = {
+  goldenFileCreated: boolean,
+  testSucceeded: boolean,
+}
+
+function goldenTest(
+  goldenFile: string,
+  testResult: string,
+  options?: {
+    acceptNew?: boolean,
+  },
+): TestResult {
+  const acceptNew = options !== undefined && options.acceptNew !== undefined ? options.acceptNew : false;
+  let goldenFileCreated;
+  let testSucceeded;
+
+  if (! acceptNew && fs.existsSync(path.resolve(__dirname, goldenFile))) {
+    goldenFileCreated = false;
+
+    const goldenContents = fs.readFileSync(path.resolve(__dirname, goldenFile), { encoding: "utf8" });
+    if (goldenContents === testResult) {
+      testSucceeded = true;
+    } else {
+      testSucceeded = false;
+    }
+  } else {
+    fs.writeFileSync(path.resolve(__dirname, goldenFile), testResult, { encoding: "utf8" });
+    testSucceeded = true;
+    goldenFileCreated = true;
+  }
+
+  return { goldenFileCreated, testSucceeded };
+}
+
+const testResult = goldenTest("golden_files/test1.txt", "1");
+console.log(testResult);
