cde98a9e308fa65e5cec20c714f7623e3b66c484
rubenpieters <rubenpietersgithub@gmail.com>
2018-12-20 10:13:57 +0100

    re-add static file server

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npx ts-node -r tsconfig-paths/register src/server/main.ts
---
+++ src/server/main.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import path from "path";
+
+main();
+
+function main(): void {
+  console.log("Server started");
+
+  // parse port from env or default
+  let parsedPort: number = 8080;
+  const envPort: string | undefined = process.env["PORT"];
+  if (envPort !== undefined) {
+    parsedPort = Number(envPort);
+  }
+
+  // initialize server
+  mkServer(parsedPort);
+}
+
+function mkServer(port: number): void {
+  const distFolder: string = path.join(__dirname, "..", "..", "dist");
+  const jsFolder: string = path.join(distFolder, "js");
+  const indexFile: string = path.join(distFolder, "index.html");
+
+  const app = express();
+
+  console.log(`dist folder: ${distFolder}`);
+
+  app.get("/", (_req, res) => { res.sendFile(indexFile); });
+  app.use("/js", express.static(jsFolder));
+
+  const server = app.listen(port, () => { console.log(`listening on ${port}`); });
+}
