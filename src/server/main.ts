ba187c4a6857513058eff52c5b15fe243f484b8d
rubenpieters <rubenpietersgithub@gmail.com>
2018-12-20 11:36:06 +0100

    serve textures folder

--- src/server/main.ts
+++ src/server/main.ts
@@ -20,6 +20,7 @@ function main(): void {
 function mkServer(port: number): void {
   const distFolder: string = path.join(__dirname, "..", "..", "dist");
   const jsFolder: string = path.join(distFolder, "js");
+  const texturesFolder: string = path.join(distFolder, "textures");
   const indexFile: string = path.join(distFolder, "index.html");
 
   const app = express();
@@ -28,6 +29,7 @@ function mkServer(port: number): void {
 
   app.get("/", (_req, res) => { res.sendFile(indexFile); });
   app.use("/js", express.static(jsFolder));
+  app.use("/textures", express.static(texturesFolder));
 
   const server = app.listen(port, () => { console.log(`listening on ${port}`); });
 }
