b3e607b73b1d979913cb7120771350ec668ff7af
rubenpieters <rubenpietersgithub@gmail.com>
2019-01-19 12:02:05 +0100

    addText func

--- src/app/phaser/datasprite.ts
+++ src/app/phaser/datasprite.ts
@@ -1,3 +1,6 @@
+import { Position } from "../util/position";
+import { GameRefs } from "../states/game";
+
 export interface DataSprite<Data> extends Phaser.Sprite {
   data: Data
   props?: {
@@ -9,4 +12,31 @@ export interface DataSprite<Data> extends Phaser.Sprite {
     // but the pointer has not been lifted up yet
     selecting: boolean,
   }
+}
+
+export function addText<Data>(
+  gameRefs: GameRefs,
+  sprite: DataSprite<Data>,
+  pos: Position,
+  btnString: string,
+  txtColor: string,
+  fontSize: number,
+) {
+  const btnText = gameRefs.game.add.text(
+    0, 0, btnString, {
+      fill: txtColor,
+      fontSize,
+      boundsAlignH: "center",
+      boundsAlignV: "middle",
+    }
+  );
+  btnText.setTextBounds(0, 0, pos.xMax - pos.xMin, pos.yMax - pos.yMin);
+  sprite.addChild(btnText);
+  
+  sprite.events.onKilled.add(() => {
+    btnText.destroy();
+  });
+  sprite.events.onDestroy.add(() => {
+    btnText.destroy();
+  });
 }
